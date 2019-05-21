6914bbc6f45d41302192c3700ad4480a10f03083
thinkforY <2321149783@qq.com>
2019-05-21 22:43:36 +0800

    路由和控制器

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('admin')->namespace('Admin')->group(function () {
+	Route::prefix('article')->group( function () {
+		Route::get('index', 'ArticleController@index');
+		Route::get('store', 'ArticleController@store');
+		Route::get('edit/{id}', 'ArticleController@edit');
+	});
+});
+Route::prefix('home')->namespace('Home')->group(function () {
+    Route::prefix('article')->group(function () {
+        Route::get('index', 'ArticleController@index');
+        Route::get('create', 'ArticleController@create');
+        Route::post('store', 'ArticleController@store');
+    });
+    Route::prefix('tag')->group(function () {
+        Route::get('index', 'TagController@index');
+        Route::get('create', 'TagController@create');
+        Route::post('store', 'TagController@store');
+    });
+});
