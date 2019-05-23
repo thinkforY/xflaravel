cc4fa4601e6c407ea282b220c4e7b65da641efc7
thinkforY <2321149783@qq.com>
2019-05-23 22:19:10 +0800

    表迁移

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
-            'collation' => 'utf8',
+            'collation' => 'utf8_general_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
